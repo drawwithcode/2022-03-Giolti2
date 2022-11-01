e3f1f526d7d1cc89c9463ca8189ed3482ded01f4
Elisa De Benedettis <105230322+Giolti2@users.noreply.github.com>
2022-11-01 20:09:48 +0100

    Eppur si muove
    -polaroids can now be moved
    -clicking on a polaroid brings it on top of the stack
    -added a red circle indicating where the polaroid is considered "clicked"

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
-const ANGLE_JITTER = 5;
-const X_JITTER = 0.2;
+const ANGLE_JITTER = 10;
+const X_JITTER = 0.3;
 const Y_JITTER = 0.2;
+
+let dragPhoto;
+let dragOffX;
+let dragOffY;
 
 class Photo{
 	constructor(img) {
@@ -22,19 +26,50 @@ class Photo{
 		rotate(this.angle);
 
 		rect(-videoHeight * 0.55, -videoHeight * 0.55, videoHeight * 1.1, videoHeight * 1.25);
-		
+
+		push()
 		scale(-1, 1)
 		image(this.img, -videoHeight * 0.5, -videoHeight * 0.5);
+		pop()
+
+		noFill()
+		stroke(255, 0, 0)
+
+		circle(0, 0, videoHeight*1.1)
 
 		pop()
+	}
+
+	mouseOn() {
+		return dist(mouseX, mouseY, this.x, this.y) < videoHeight * 0.55
 	}
 }
 
 const FLASH_DURATION = 400;
 
-class FLash{
+class Flash{
 	constructor() {
 		this.time = 0;
+		this.startTime = 0;
+	}
+
+	start() {
+		this.time = FLASH_DURATION;
+		this.startTime = millis();
+	}
+
+	update() {
+		push()
+
+		noStroke()
+
+		let alpha = 1 - (millis() - this.startTime) / FLASH_DURATION;
+
+		fill(color(255, 255, 255, alpha * 255))
+
+		rect(0, 0, width, height)
+
+		pop()
 	}
 }
 
@@ -42,6 +77,8 @@ const videoWidth = 640;
 const videoHeight = 360;
 
 let capture;
+
+let flash = new Flash();
 
 let photos = [];
 
@@ -78,11 +115,42 @@ function draw() {
 	photos.forEach(function (photo) {
 		photo.draw();
 	})
+
+	flash.update();
+}
+
+function mousePressed() {
+	for (let i = photos.length - 1; i >= 0; i--){
+		if (photos[i].mouseOn()) {
+			dragPhoto = photos.splice(i, 1)[0];
+			dragOffX = dragPhoto.x - mouseX;
+			dragOffY = dragPhoto.y - mouseY;
+			photos.push(dragPhoto);
+
+			break;
+		}
+	}
+}
+
+function mouseDragged() {
+	if (dragPhoto) {
+		dragPhoto.x = mouseX + dragOffX;
+		dragPhoto.y = mouseY + dragOffY;
+	}
+}
+
+function mouseReleased() {
+	dragPhoto = null;
+	dragOffX = 0;
+	dragOffY = 0;
 }
 
 function smile() {
 	if (capture.loadedmetadata) {
-		let img = capture.get(videoWidth * 7 / 32, 0, videoWidth * 18 / 32, videoHeight);
-		photos.push(new Photo(img))
+		flash.start();
+		setTimeout(function () {
+			let img = capture.get(videoWidth * 7 / 32, 0, videoWidth * 18 / 32, videoHeight);
+			photos.push(new Photo(img))
+		}, 100)
 	}
 }
